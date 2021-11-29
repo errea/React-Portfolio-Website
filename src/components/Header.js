fc2a4c038f68d03e5cc0894a7cf4c533d0c9a8b0
errea <erreakezy@gmail.com>
2021-11-29 23:20:40 +0100

    Add Header.js to component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+const Navbar = () => {
+    return (
+    <h1>
+        Pretty code by ingenious person
+    </h1>
+    )
+}
+
+export default Navbar
