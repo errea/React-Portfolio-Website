3546d343ffc40776d79367c701cbbf98fcaab3df
errea <erreakezy@gmail.com>
2021-11-29 23:23:07 +0100

    Add header text and paragraph to div tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
-const Navbar = () => {
+const Header = () => {
     return (
-    <h1>
+    <div>
+        <h1>
         Pretty code by ingenious person
-    </h1>
+        </h1>
+        <p>
+        Hello I am a software developer! I can help you build a product, featre or website. 
+        Look through some of my work and experience! If you like what you 
+        see and have project you need coded, don’t hesistate to contact me.
+        </p>
+    </div>
     )
 }
 
-export default Navbar
+export default Header
