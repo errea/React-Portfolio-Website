f78353ea23a10c62f7fd427242c4b705a2004c55
errea <erreakezy@gmail.com>
2021-11-30 23:31:10 +0100

    Import config for tailwind css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 function App() {
   return (
-  <Navbar />
+  <div>
+    <Navbar />
+    <Header />
+    {/* <div className="App" className="p-6 items-center justify-center">
+        <h1 className="text-blue-400 font-extrabold">Hello World!</h1>
+        <p className="tracking-widest">This is my first React App.</p>
+    </div> */}
+  </div>
+  
   );
 }
 
