83a650d4f901193f93f8b2791ae867bb94060963
errea <erreakezy@gmail.com>
2021-12-02 22:36:44 +0100

    Add import for bestwork section

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <h1 className="text-blue-400 font-extrabold">Hello World!</h1>
         <p className="tracking-widest">This is my first React App.</p>
     </div> */}
-
+    <Bestwork />
   </div>
   
   );
