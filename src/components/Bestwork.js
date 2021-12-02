758b55bc9068aa4e318ef4721cebeeda79fd1425
errea <erreakezy@gmail.com>
2021-12-02 15:56:08 +0100

    Add h1 tag and img tag to div grid

--- src/components/Bestwork.js
+++ src/components/Bestwork.js
@@ -3,12 +3,26 @@ import React from 'react';
 const Bestwork = () => {
     return (
         <div class="grid grid-cols-3 gap-4">
+        <div>
+            <h1>
+                hbsbcsucu
+            </h1>
+            <img src="https://" alt="" />
+        </div>
+        <div>
+            <h1>
+                hbsbcsucu
+            </h1>
+            <img src="https://" alt="" />
+        </div>
         <div>1</div>
         <div>1</div>
         <div>1</div>
         <div>1</div>
         <div>1</div>
         <div>1</div>
+        <div>1</div>
+        <div>1</div>        
         <div>9</div>
       </div>
     )
