e388abc59504bd75f2ba079fc7bcbba357c5f386
errea <erreakezy@gmail.com>
2021-12-03 17:58:44 +0100

    Add grid div + modify div img tag

--- src/components/Bestwork.js
+++ src/components/Bestwork.js
@@ -33,12 +33,42 @@ const Bestwork = () => {
             </h1>
             <img src="https://" alt="" />
         </div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>        
-        <div>9</div>
+        <div>
+            <h1>
+                hbsbcsucu
+            </h1>
+            <img src="https://" alt="" />
+        </div>
+        <div>
+            <h1>
+                hbsbcsucu
+            </h1>
+            <img src="https://" alt="" />
+        </div>
+        <div>
+            <h1>
+                hbsbcsucu
+            </h1>
+            <img src="https://" alt="" />
+        </div>
+        <div>
+            <h1>
+                hbsbcsucu
+            </h1>
+            <img src="https://" alt="" />
+        </div>
+        <div>
+            <h1>
+                hbsbcsucu
+            </h1>
+            <img src="https://" alt="" />
+        </div>       
+        <div>
+            <h1>
+                hbsbcsucu
+            </h1>
+            <img src="https://" alt="" />
+        </div>
       </div>
     )
 }
